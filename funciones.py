990e3770d83b50594dc01db4ee89fad1bc7bcd51
Jose Julio <jjpeajar@epsg.upv.es>
2022-03-07 13:11:25 +0100

    checkpoint 6

--- funciones.py
+++ funciones.py
@@ -8,10 +8,14 @@ def encontrar_menores(diccionario,letra):
     Returns:
       resultado: ej. ['AUNQUE','ABINAR']
     """
+    """ 
+    PARA SOLUCIONAR EL ERROR EN EL TEST HE DECLARADO EL ARRAY RESULTADO AL PRINCIPIO DE LA FUNCIÓN PARA QUE NO SE INICIALICE VAÍO CADA VEZ
+    QUE ENCUENTRE UNA PALABRA
+    """
+    resultado=[]
     for clave in diccionario:
         for palabra in diccionario[clave]:
             if palabra[0] < letra:
-                resultado=[]
                 resultado.append(palabra)
     return resultado
 
@@ -25,13 +29,17 @@ def add_client(clients_list,nif,name,address,phone,email):
       phone
       email
     """
-    clients_list[nif] = {
-        nif: {'name': name,
+
+    """ 
+    PARA SOLUCIONAR EL ERROR EN EL TEST HE CREADO EL OBJETO CLIENTE APARTE Y LO HE AÑADIDO AL DICCIONARIO USANDO LA FUNCIÓN UPDATE
+    """
+    cliente = {'name': name,
               'address': address,
               'phone': phone,
               'email': email
         }
-    }
+    clients_list.update({nif: cliente})
+        
 
 def repartir_cartas(cartas_iniciales,repeticiones):
     """Dada una baraja de cartas iniciales y un nÃºmero de repeticiones, esta funciÃ³n selecciona 5 cartas aleatorias de esta baraja y las mete en un diccionario llamado combinaciones. El proceso se repite tantas veces como repeticiones se indiquen.
@@ -43,13 +51,16 @@ def repartir_cartas(cartas_iniciales,repeticiones):
     """    
     combinaciones={}
     for i in range(1,repeticiones+1):
-        cartas_aleatorias = cartas_iniciales 
-        combinaciones["repeticion"+str(i)]=[]
+        cartas_aleatorias = cartas_iniciales
+        cartas_repe=[]
         for j in range(0,5):
             carta=random.choice(cartas_aleatorias)
-            combinaciones["repeticion"+str(i)].append(carta)
+            cartas_repe.append(carta)
             cartas_aleatorias.remove(carta)
+        
+        combinaciones.update({"repeticion"+str(i): cartas_repe})
 
+    print(combinaciones)
     return combinaciones
 
     
